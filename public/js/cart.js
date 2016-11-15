e9972013ddabe86b099737caae83c4c656b8a8b8
Aaron Ballard <mraballard@gmail.com>
2016-11-15 16:33:58 -0300

    changed update cart function in  service to correctly update quantity if item is already in the cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -5,6 +5,7 @@
 
   function Cart(){
     var cart = {};
+    cart.total;
     cart.emptyCart = function(){
       cart.items = [];
       console.log('$cart.items should be empty:');
@@ -14,14 +15,13 @@
         var indexOfProductInCart = -1;
         if (cart.items.length > 0) { // if cart is not empty, check to see if product is already in cart
           indexOfProductInCart = cart.items.findIndex(function(el) {
-            return el.product._id === item.id;
+            console.log(el);
+            return el.product.id === item.id;
           });
         }
         if (indexOfProductInCart === -1) {
             console.log('adding to cart');
-            cart.items.push({product: item, quantity: Number(quantity)})
-            console.log(cart.items);
-            console.log('cart length: '+cart.items.length);
+            cart.items.push({product: item, quantity: Number(quantity), subtotal: (item.price * quantity)})
         } else {
           cart.items[indexOfProductInCart].quantity += Number(quantity);
         }
@@ -32,12 +32,20 @@
             return el.product._id === item.id;
           });
           cart.items[indexOfProductInCart].quantity = Number(quantity);
+          cart.items[indexOfProductInCart].subtotal = (item.price * Number(quantity));
     }
     cart.remove = function(item) {
       indexOfProductInCart = cart.items.findIndex(function(el) {
             return el.product._id === item.id;
           });
           cart.items.splice(indexOfProductInCart, 1);
+    }
+
+    cart.calculateTotal = function() {
+      cart.total = 0;
+      cart.items.forEach(function(el){
+        cart.total += el.subtotal;
+      })
     }
 
     cart.emptyCart();
